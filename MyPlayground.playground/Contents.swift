3a505f934e7b0bdb18d81b870057f8d6bbf02fd3
Vincent Giles <vin@vgiles.net>
2018-03-06 19:26:20 +1100

    Doing a heaps fun have

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,4 +2,18 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var hello = "Hello, playground"
+var hi = "Oh, hi there"
+var calc = 0
+var aNumber = 42.8
+var aNotherNumber = 4.3
+var calcu = "42.8 multiplied by 4.3 is: \(aNumber * aNotherNumber)"
+
+print(calcu)
+
+
+
+print(hello)
+print(hi)
+print(aNumber)
+
