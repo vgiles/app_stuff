90d7d78e5c8058dcf7a932e8081718f1c282f37a
Vincent Giles <vin@vgiles.net>
2018-03-07 11:45:11 +1100

    starting now

---
+++ NoiseFloor/NoiseFloor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoiseFloor
+//
+//  Created by Vincent Giles on 7/3/18.
+//  Copyright © 2018 Vincent Giles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
